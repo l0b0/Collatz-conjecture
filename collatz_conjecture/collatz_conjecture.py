9decaf6d59f606a93b56c6d06e0082b05da3280b
Victor Engmark <victor.engmark@gmail.com>
2017-03-28 22:01:09 +0100

    Generate trivial graph

--- collatz_conjecture/collatz_conjecture.py
+++ collatz_conjecture/collatz_conjecture.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 
-class PythonSkeleton(object):
-    def hello(self):
-        print('Hello world!')
+class GraphGenerator(object):
+    def generate(self, number):
+        if number == 1:
+            return [1]
--- tests/test_collatz_conjecture.py
+++ tests/test_collatz_conjecture.py
@@ -1,14 +1,12 @@
 from __future__ import print_function
+
 import unittest
-import mock
 
 from collatz_conjecture import collatz_conjecture
-from tests.utilities import module_function_name
 
 
-class TestPythonSkeleton(unittest.TestCase):
-    @mock.patch(module_function_name(print))
-    def test_should_print_hello_world(self, mock_print):
-        skeleton = collatz_conjecture.PythonSkeleton()
-        skeleton.hello()
-        mock_print.assert_called_once_with('Hello world!')
+class TestCollatzConjectureGraphGenerator(unittest.TestCase):
+    def test_should_generate_trivial_graph(self):
+        dot_generator = collatz_conjecture.GraphGenerator()
+        graph = dot_generator.generate(1)
+        self.assertEqual(graph, [1])
