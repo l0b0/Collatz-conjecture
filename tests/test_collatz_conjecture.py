f171cca4cc9cb44ae47b9595a276a0d10a05137f
Victor Engmark <victor.engmark@gmail.com>
2017-03-28 22:16:01 +0100

    Simplify tests

--- tests/test_collatz_conjecture.py
+++ tests/test_collatz_conjecture.py
@@ -6,17 +6,17 @@ from collatz_conjecture import collatz_conjecture
 
 
 class TestCollatzConjectureGraphGenerator(unittest.TestCase):
+    def setUp(self):
+        self.dot_generator = collatz_conjecture.GraphGenerator()
+
     def test_should_generate_trivial_graph(self):
-        dot_generator = collatz_conjecture.GraphGenerator()
-        graph = dot_generator.generate(1)
+        graph = self.dot_generator.generate(1)
         self.assertEqual(graph, [1])
 
     def test_should_generate_graph_with_an_even_starting_point(self):
-        dot_generator = collatz_conjecture.GraphGenerator()
-        graph = dot_generator.generate(2)
+        graph = self.dot_generator.generate(2)
         self.assertEqual(graph, [2, 1])
 
     def test_should_generate_graph_with_an_odd_starting_point(self):
-        dot_generator = collatz_conjecture.GraphGenerator()
-        graph = dot_generator.generate(3)
+        graph = self.dot_generator.generate(3)
         self.assertEqual(graph, [3, 10, 5, 16, 8, 4, 2, 1])
