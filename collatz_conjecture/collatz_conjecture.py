a5d5f724b8bf8b33b12189672a4f46136f43eff0
Victor Engmark <victor.engmark@gmail.com>
2017-03-28 22:17:30 +0100

    Fail if the starting point is below one

--- collatz_conjecture/collatz_conjecture.py
+++ collatz_conjecture/collatz_conjecture.py
@@ -3,6 +3,8 @@ from __future__ import print_function
 
 class GraphGenerator(object):
     def generate(self, number):
+        assert number >= 1
+
         graph = []
         while number != 1:
             graph.append(number)
--- tests/test_collatz_conjecture.py
+++ tests/test_collatz_conjecture.py
@@ -20,3 +20,6 @@ class TestCollatzConjectureGraphGenerator(unittest.TestCase):
     def test_should_generate_graph_with_an_odd_starting_point(self):
         graph = self.dot_generator.generate(3)
         self.assertEqual(graph, [3, 10, 5, 16, 8, 4, 2, 1])
+
+    def test_should_fail_with_a_starting_point_below_one(self):
+        self.assertRaises(AssertionError, self.dot_generator.generate, 0)
